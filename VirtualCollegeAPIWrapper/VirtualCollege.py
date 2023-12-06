9d038f28f6c2dbb75efa347d3a9a9963454f6dac
Pietro Convalle <pietro.convalle@oldham.ac.uk>
2023-12-06 14:22:50 +0000

    Fixed export_report, added export_add method

--- VirtualCollegeAPIWrapper/VirtualCollege.py
+++ VirtualCollegeAPIWrapper/VirtualCollege.py
@@ -6,6 +6,7 @@ import json
 import jwt
 from datetime import datetime, timedelta
 from dataclasses import dataclass
+from Columns import Columns
 
 
 @dataclass
@@ -20,9 +21,31 @@ class InvalidReport(Exception):
 
     def __str__(self):
         return f"Invalid Report Type, valid reports: [{self.message}]"
+    
+@dataclass
+class InvalidExportColumns(Exception):
+    message: str = ""
+
+    def __str__(self):
+        return f"One or more columns are invalid, valid columns: [{self.message}]"
 
 
 class VirtualCollegeAPI:
+    report_types = ("modules", "users", "evaluation", "event")
+    report_paths = {
+            "modules": "/reporting/learn/moduleAllocation",
+            "users": "/reporting/core/users",
+            "evaluation": "/reporting/learn/evaluationQuestion",
+            "event": "/reporting/events/eventSession",
+        }
+    report_columns = {
+            "modules": Columns.columns_module_export,
+            "users": Columns.columns_user_export,
+            "evaluation": Columns.columns_evaluation_export,
+            "event": Columns.columns_evaluation_export,
+        }
+    
+
     def get_header(self):
         # request a new token if it has less than a minute of lifetime
         if self.expiry and self.expiry <= (datetime.now() - timedelta(0, 60)):
@@ -75,23 +98,43 @@ class VirtualCollegeAPI:
         self.get_authorisation_token()
         self.get_access_token()
 
-    def export_report(self, reportID):
-        request_body = {"columns": ["FirstName", "LastName"]}
+    def export_report(self, report_type, reportID, columns: list[str], fp):
+        if report_type not in self.report_types:
+            raise InvalidReport(", ".join(self.report_types))
+        
+        if any(map(lambda x: x not in self.report_columns[report_type], columns)):
+            raise InvalidExportColumns(", ".join(self.report_columns[report_type]))
+
+        request_body = {"columns": columns}
+        data = self.post_request(
+            f"{self.report_paths[report_type]}/{reportID}/export", request_body
+        )
+
+        data = json.loads(data)
+        is_first_line = True
+        for row in data.get("rows", {}):
+            cells = row.get("cells", [])
+            if cells:
+                if is_first_line:
+                    fp.write(",".join(map(lambda c : c["column"], cells))+"\n")
+                    is_first_line = False
+                fp.write(",".join(map(lambda c : c["value"], cells))+"\n")
+
+    
+    def export_add(self, report_type, name):
+        if report_type not in self.report_types:
+            raise InvalidReport(", ".join(self.report_types))
+        
+        request_body = {"name": name}
         return self.post_request(
-            f"/reporting/learn/moduleAllocation/{reportID}/export", request_body
+            f"{self.report_paths[report_type]}", request_body
         )
 
     def list_reports(self, report_type):
-        report_types = ("modules", "users", "evaluation", "event")
-        if report_type not in report_types:
-            raise InvalidReport(", ".join(report_types))
-        reports = {
-            "modules": "/reporting/learn/moduleAllocation",
-            "users": "/reporting/core/users",
-            "evaluation": "/reporting/learn/evaluationQuestion",
-            "event": "/reporting/events/eventSession",
-        }
-        return self.get_request(reports[report_type])
+        if report_type not in self.report_types:
+            raise InvalidReport(", ".join(self.report_types))
+            
+        return self.get_request(self.report_paths[report_type])
 
     def search_user(self, search):
         request_body = {
